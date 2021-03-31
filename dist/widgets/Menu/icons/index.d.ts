b3ea85490541114dd50629170ed13edefb2a1945
panther-swap <admin@pantherswap.com>
2021-03-31 14:12:31 +0800

    build: Publish

--- dist/widgets/Menu/icons/index.d.ts
+++ dist/widgets/Menu/icons/index.d.ts
@@ -16,3 +16,5 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as ReferralIcon } from "./Referral";
+export { default as ShieldIcon } from "./Shield";
