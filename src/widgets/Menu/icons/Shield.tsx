6bc888a21eebb663106f2d062db8488378a80289
panther-swap <admin@pantherswap.com>
2021-03-31 13:43:49 +0800

    refactor: Change menu icons

--- src/widgets/Menu/icons/Shield.tsx
+++ src/widgets/Menu/icons/Shield.tsx
@@ -4,9 +4,9 @@ import { SvgProps } from "../../../components/Svg/types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 24 24" {...props}>
+    <Svg viewBox="0 0 28 28" {...props}>
       <path d="M0 0h24v24H0V0z" fill="none" />
-      <path d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
+      <path d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z" />
     </Svg>
   );
 };
