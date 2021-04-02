6e031887d87e9b4f6774f279e5c52dbe2109ffe9
panther-swap <admin@pantherswap.com>
2021-04-02 10:12:36 +0800

    refactor: Update spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,52 +1,75 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import PanIcon from "./PanIcon";
-import PancakeIcon from "./PancakeIcon";
+// import PanIcon from "./PanIcon";
+// import PancakeIcon from "./PancakeIcon";
+import LogoIcon from "../Svg/Icons/Logo";
 import { SpinnerProps } from "./types";
 
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
+// const rotate = keyframes`
+//   from {
+//     transform: rotate(0deg);
+//   }
+//   to {
+//     transform: rotate(360deg);
+//   }
+// `;
+//
+// const float = keyframes`
+// 	0% {
+// 		transform: translatey(0px);
+// 	}
+// 	50% {
+// 		transform: translatey(10px);
+// 	}
+// 	100% {
+// 		transform: translatey(0px);
+// 	}
+// `;
 
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
+const pulse = keyframes`
+  from {
+    transform: scale3d(1, 1, 1);
+  }
+
+  50% {
+    transform: scale3d(1.05, 1.05, 1.05);
+  }
+
+  to {
+    transform: scale3d(1, 1, 1);
+  }
 `;
 
 const Container = styled.div`
   position: relative;
 `;
 
-const RotatingPancakeIcon = styled(PancakeIcon)`
+// const RotatingPancakeIcon = styled(PancakeIcon)`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   animation: ${rotate} 2s linear infinite;
+//   transform: translate3d(0, 0, 0);
+// `;
+//
+// const FloatingPanIcon = styled(PanIcon)`
+//   animation: ${float} 6s ease-in-out infinite;
+//   transform: translate3d(0, 0, 0);
+// `;
+
+const PulseLogoIcon = styled(LogoIcon)`
   position: absolute;
   top: 0;
   left: 0;
-  animation: ${rotate} 2s linear infinite;
-  transform: translate3d(0, 0, 0);
-`;
-
-const FloatingPanIcon = styled(PanIcon)`
-  animation: ${float} 6s ease-in-out infinite;
+  animation: ${pulse} 2s ease-in-out infinite;
   transform: translate3d(0, 0, 0);
 `;
 
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <PulseLogoIcon width={`${size * 0.5}px`} />
+      {/*<FloatingPanIcon width={`${size}px`} />*/}
     </Container>
   );
 };
