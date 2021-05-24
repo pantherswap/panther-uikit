95bdf0328678a6d9d93c7f308b1ca2f523f1c904
panther-swap <admin@pantherswap.com>
2021-05-24 10:59:36 +0800

    chore: Add new icon

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -26,3 +26,4 @@ export { default as ListingIcon } from "./Listing";
 export { default as FaucetIcon } from "./Faucet";
 export { default as AirdropIcon } from "./Airdrop";
 export { default as ChartIcon } from "./Chart";
+export { default as FeatureIcon } from "./Feature";
